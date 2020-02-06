00a840c89e17b7207c610fa7d0a7e5960c721121
Andrey Prokopenko <9478806+andrey42@users.noreply.github.com>
2020-02-06 14:52:54 +0100

    Update hetzner-vps-debian10-setup.sh

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # zfs-hetzner-vm
-script to install Debian 10 with ZFS root on Hetzner VPS
+
+Fully automatic, unattended script to install Debian 10 with ZFS root on Hetzner VPS.
+__WARNING:__ all data on the disk will be destroyed.
+
+How to use: add SSH key to the rescue console, set it OS to linux64, then press mount rescue and power cycle
+Next, connect via SSH to rescue console, download the script and run it:
+````
+wget https://raw.githubusercontent.com/andrey42/zfs-hetzner-vm/master/hetzner-vps-debian10-setup.sh && \
+chmod 755 hetzner-vps-debian10-setup.sh && \
+./hetzner-vps-debian10-setup.sh
+````
+Answer script questions about desired hostname and ZFS ARC cache size
+To cope with network failures its higly recommended to run the script inside screen console, type `man screen` for more info
+````
+screen -dmS zfs
+screen -r zfs
+````
+To detach from screen console, hit Ctrl-d then a
+To reattach, type `screen -r zfs`
+
+Upon succesfull run, the script will reboot system, and you will be able to login into it, using the same SSH key you have used within rescue console
+
--- hetzner-vps-debian10-setup.sh
+++ hetzner-vps-debian10-setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+: <<'end_header_info'
+(c) Andrey Prokopenko job@terem.fr
+fully automatic script to install Debian 10 with ZFS root on Hetzner VPS
+WARNING: all data on the disk will be destroyed
+How to use: add SSH key to the rescue console, set it OS to linux64, then press mount rescue and power sysle
+Next, connect via SSH to console, and run the script 
+Answer script questions about desired hostname and ZFS ARC cache size
+To cope with network failures its higly recommended to run the script inside screen console
+screen -dmS zfs
+screen -r zfs
+To detach from screen console, hit Ctrl-d then a
+end_header_info
+
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -8,9 +21,78 @@ function chroot_execute {
   chroot /mnt bash -c "$1"
 }
 
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
-update-locale LANG=en_US.UTF-8 LANGUAGE=en_US:en
+function update_zed_cache_Debian {
+  chroot_execute "mkdir /etc/zfs/zfs-list.cache"
+  chroot_execute "touch /etc/zfs/zfs-list.cache/rpool"
+  chroot_execute "ln -s /usr/lib/zfs-linux/zed.d/history_event-zfs-list-cacher.sh /etc/zfs/zed.d/"
+
+  # Assumed to be present by the zedlet above, but missing.
+  # Filed issue: https://github.com/zfsonlinux/zfs/issues/9945.
+  #
+  chroot_execute "mkdir /run/lock"
+
+  chroot_execute "zed -F &"
+
+  # We could pool the events via `zpool events -v`, but it's much simpler to just check on the file.
+  #
+  local success=0
+  
+  if [[ ! -e /mnt/etc/zfs/zfs-list.cache/rpool ]] || [[ -e /mnt/etc/zfs/zfs-list.cache/rpool && (( $(ls -l /mnt/etc/zfs/zfs-list.cache/rpool 2> /dev/null | cut -d ' ' -f 5) == 0 )) ]]; then
+    # Takes around half second on a test VM.
+    #
+    chroot_execute "zfs set canmount=noauto rpool"
+
+    SECONDS=0
+
+    while (( SECONDS++ <= 120 )); do
+      if [[ -e /mnt/etc/zfs/zfs-list.cache/rpool ]] && (( "$(ls -l /mnt/etc/zfs/zfs-list.cache/rpool | cut -d ' ' -f 5)" > 0 )); then
+        success=1
+        break
+      else
+        echo " no data available yet"
+        ls /mnt/etc/zfs/zfs-list.cache/ -al
+        sleep 1
+      fi      
+    done
+  else 
+    success=1
+  fi
+
+  if (( success != 1 )); then
+    echo "Error: The ZFS cache hasn't been updated by ZED!"
+    exit 1
+  fi
+
+  chroot_execute "pkill zed"
+
+  sed -Ei 's|/mnt/?|/|' /mnt/etc/zfs/zfs-list.cache/rpool
+}
+
+
+export LC_ALL=en_US.UTF-8
+
+if [[ ! -r /root/.ssh/authorized_keys ]]; then
+  echo "SSH pubkey file is absent, please add it to the rescue system setting, then reboot into rescue system and run the script"
+  exit 1
+fi
+
+read -p "Enter desired hostname [terem]: " v_hostname
+v_hostname=${v_hostname:-terem}
+
+read -p "Enter desired ZFS ARC max cache size in bytes [250000000]: " v_zfs_arc_max
+v_zfs_arc_max=${v_zfs_arc_max:-250000000}
+
+echo "NOTE: password login via SSH will be disabled for all accounts"
+read -p "Enter root password [test1234]: " v_root_password
+v_root_password=${v_root_password:-test1234}
+
+
+echo "===========remove unused kernels in rescue system========="
+for kver in $(ls /lib/modules/ -1 | grep -v "$(uname -r)"); do 
+  apt purge --yes linux-headers-$kver
+  apt purge --yes linux-image-$kver  
+done
+
 
 echo "======= installing zfs on rescue system =========="
 echo "zfs-dkms zfs-dkms/note-incompatible-licenses note true" | debconf-set-selections
@@ -23,7 +105,7 @@ zfs --version
 echo "======= partitioning the disk =========="
 DISK=/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-0-0-0
 
-sgdisk --zap-all $DISK
+wipefs --all $DISK
 sgdisk -a1 -n1:24K:+1000K            -t1:EF02 $DISK # MBR area
 sgdisk -n2:0:+512M                   -t2:BF01 $DISK # Boot pool
 sgdisk -n3:0:0                       -t3:BF01 $DISK # Root pool
@@ -43,11 +125,11 @@ zpool create -o ashift=12 \
 zfs create -o canmount=off -o mountpoint=none rpool/ROOT
 zfs create -o canmount=off -o mountpoint=none bpool/BOOT
 
-zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/ubuntu
-zfs mount rpool/ROOT/ubuntu
+zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/debian
+zfs mount rpool/ROOT/debian
 
-zfs create -o canmount=noauto -o mountpoint=/boot bpool/BOOT/ubuntu
-zfs mount bpool/BOOT/ubuntu
+zfs create -o canmount=noauto -o mountpoint=/boot bpool/BOOT/debian
+zfs mount bpool/BOOT/debian
 
 zfs create                                 rpool/home
 zfs create -o mountpoint=/root             rpool/home/root
@@ -77,7 +159,7 @@ zfs set devices=off rpool
 
 echo "======= setting up the network =========="
 
-echo terem > /mnt/etc/hostname
+echo $v_hostname > /mnt/etc/hostname
 
 cat > /mnt/etc/hosts <<CONF
 127.0.1.1 terem terem
@@ -169,6 +251,7 @@ tzdata tzdata/Zones/Europe select Vienna
 CONF'
 
 chroot_execute "dpkg-reconfigure locales -f noninteractive"
+echo -e "LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8\n" >> /mnt/etc/environment
 chroot_execute "apt install -qq --yes keyboard-configuration console-setup"
 chroot_execute "dpkg-reconfigure keyboard-configuration -f noninteractive"
 chroot_execute "dpkg-reconfigure console-setup -f noninteractive"
@@ -176,16 +259,26 @@ chroot_execute "dpkg-reconfigure console-setup -f noninteractive"
 chroot_execute "rm -f /etc/localtime /etc/timezone"
 chroot_execute "dpkg-reconfigure -f noninteractive tzdata"
 
+echo "======= installing new kernel============="
+#chroot_execute "update-grub"
+#chroot_execute "apt install --yes -t buster-backports linux-image-amd64"
+#chroot_execute "apt install --yes -t buster-backports firmware-linux firmware-linux-nonfree"
+chroot_execute "apt install --yes linux-headers-4.19.0-6"
+
 echo "======= installing zfs packages =========="
 chroot_execute 'echo "zfs-dkms zfs-dkms/note-incompatible-licenses note true" | debconf-set-selections'
 chroot_execute "apt install --yes zfs-initramfs zfs-dkms"
 
-echo "======= installing ssh server and aux packages =========="
-chroot_execute "apt install --yes openssh-server net-tools wget curl nano"
-
-echo "======= setting credentials for root using resque system SSH key =========="
+echo "======= setting credentials for root using rescue system SSH key =========="
 mkdir -p /mnt/root/.ssh/
 cp /root/.ssh/authorized_keys /mnt/root/.ssh/authorized_keys
+
+echo "======= set SSH auth only via public keys =========="
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /mnt/etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /mnt/etc/ssh/sshd_config
+
+echo "======= set root password =========="
+chroot_execute "echo root:'"$v_root_password"' | chpasswd"
 
 echo "======= setting up zfs services =========="
 chroot_execute "cat > /etc/systemd/system/zfs-import-bpool.service <<UNIT
@@ -210,9 +303,56 @@ chroot_execute "systemctl enable zfs-import-bpool.service"
 chroot_execute "cp /usr/share/systemd/tmp.mount /etc/systemd/system/"
 chroot_execute "systemctl enable tmp.mount"
 
+echo "========setting up zfs module parameters========"
+chroot_execute "echo options zfs zfs_arc_max=$v_zfs_arc_max >> /etc/modprobe.d/zfs.conf"
+
+echo "======= update openssh server host keys =========="
+chroot_execute "rm /etc/ssh/ssh_host_*"
+chroot_execute "dpkg-reconfigure openssh-server -f noninteractive"
+
+echo "======= setting up grub =========="
+chroot_execute "grub-install $DISK"
+
+chroot_execute "sed -i 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/g' /etc/default/grub"
+chroot_execute "sed -i 's|GRUB_CMDLINE_LINUX=\"\"|GRUB_CMDLINE_LINUX=\"root=ZFS=rpool/ROOT/debian\"|g'  /etc/default/grub"
+
+chroot_execute "sed -i 's/quiet//g' /etc/default/grub"
+chroot_execute "sed -i 's/splash//g' /etc/default/grub"
+chroot_execute "echo 'GRUB_DISABLE_OS_PROBER=true'   >> /etc/default/grub"
+
+echo "=========set up dropbear=============="
+chroot_execute "apt remove --yes cryptsetup-initramfs"
+chroot_execute "apt install --yes --no-install-recommends dropbear-initramfs"
+
+cp /root/.ssh/authorized_keys /mnt/etc/dropbear-initramfs/authorized_keys
+
+cp /mnt/etc/ssh/ssh_host_rsa_key /mnt/etc/ssh/ssh_host_rsa_key_temp
+chroot_execute "ssh-keygen -p -i -m pem -N '' -f /etc/ssh/ssh_host_rsa_key_temp"
+chroot_execute "/usr/lib/dropbear/dropbearconvert openssh dropbear /etc/ssh/ssh_host_rsa_key_temp /etc/dropbear-initramfs/dropbear_rsa_host_key"
+rm -rf /mnt/etc/ssh/ssh_host_rsa_key_temp
+
+cp /mnt/etc/ssh/ssh_host_ecdsa_key /mnt/etc/ssh/ssh_host_ecdsa_key_temp
+chroot_execute "ssh-keygen -p -i -m pem -N '' -f /etc/ssh/ssh_host_ecdsa_key_temp"
+chroot_execute "/usr/lib/dropbear/dropbearconvert openssh dropbear /etc/ssh/ssh_host_ecdsa_key_temp /etc/dropbear-initramfs/dropbear_ecdsa_host_key"
+chroot_execute "rm -rf /etc/ssh/ssh_host_ecdsa_key_temp"
+rm -rf /mnt/etc/ssh/ssh_host_ecdsa_key_temp
+
+echo "======= update initramfs =========="
+chroot_execute "update-initramfs -u -k all"
+
+echo "======= update grub =========="
+chroot_execute "update-grub"
+
+echo "======= setting up zed =========="
+#update_zed_cache_Debian
+chroot_execute "mkdir /etc/zfs/zfs-list.cache"
+chroot_execute "touch /etc/zfs/zfs-list.cache/rpool"
+chroot_execute "ln -s /usr/lib/zfs-linux/zed.d/history_event-zfs-list-cacher.sh /etc/zfs/zed.d/"
+
+
 echo "======= setting mountpoints =========="
-chroot_execute "zfs set mountpoint=legacy bpool"
-chroot_execute "echo bpool /boot zfs nodev,relatime,x-systemd.requires=zfs-import-bpool.service 0 0 > /etc/fstab"
+chroot_execute "zfs set mountpoint=legacy bpool/BOOT/debian"
+chroot_execute "echo bpool/BOOT/debian /boot zfs nodev,relatime,x-systemd.requires=zfs-import-bpool.service 0 0 > /etc/fstab"
 
 chroot_execute "zfs set mountpoint=legacy rpool/var/log"
 chroot_execute "echo rpool/var/log /var/log zfs nodev,relatime 0 0 >> /etc/fstab"
@@ -222,33 +362,6 @@ chroot_execute "zfs set mountpoint=legacy rpool/var/tmp"
 chroot_execute "echo rpool/var/tmp /var/tmp zfs nodev,relatime 0 0 >> /etc/fstab"
 chroot_execute "zfs set mountpoint=legacy rpool/tmp"
 chroot_execute "echo rpool/tmp /tmp zfs nodev,relatime 0 0 >> /etc/fstab"
-
-echo "========setting up zfs module parameters========"
-echo "options zfs zfs_arc_max=250000000" >> /etc/modprobe.d/zfs.conf
-
-echo "======= update initramfs =========="
-chroot_execute "update-initramfs -u -k all"
-
-echo "======= update openssh server host keys =========="
-chroot_execute "rm /etc/ssh/ssh_host_*"
-chroot_execute "dpkg-reconfigure openssh-server"
-
-echo "======= setting up grub =========="
-chroot_execute "grub-install $DISK"
-
-chroot_execute "sed -i 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/g' /etc/default/grub"
-chroot_execute "sed -i 's|GRUB_CMDLINE_LINUX=\"\"|GRUB_CMDLINE_LINUX=\"root=ZFS=rpool/ROOT/ubuntu\"|g'  /etc/default/grub"
-
-chroot_execute "sed -i 's/quiet//g' /etc/default/grub"
-chroot_execute "sed -i 's/splash//g' /etc/default/grub"
-chroot_execute "echo 'GRUB_DISABLE_OS_PROBER=true'   >> /etc/default/grub"
-
-chroot_execute "update-grub"
-
-echo "======= setting up zed =========="
-chroot_execute "mkdir /etc/zfs/zfs-list.cache"
-chroot_execute "touch /etc/zfs/zfs-list.cache/rpool"
-chroot_execute "ln -s /usr/lib/zfs-linux/zed.d/history_event-zfs-list-cacher.sh /etc/zfs/zed.d/"
 
 echo "======= unmounting virtual filesystems from jail =========="
 for virtual_fs_dir in dev sys proc; do
